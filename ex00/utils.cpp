efb5206f9745e18c8f9e5b0f575cf3ba40f072e8
Luca Ferro <luca.ferro@syncai.ch>
2025-10-13 12:19:14 +0200

    s

--- ex00/utils.cpp
+++ ex00/utils.cpp
@@ -2,9 +2,7 @@
 
 #include "header.hpp"
 
-bool is_space(char c) {
-  return std::isspace(static_cast<unsigned char>(c)) != 0;
-}
+bool is_space(char c) { return std::isspace(static_cast<unsigned char>(c)) != 0; }
 
 std::string removeAllWhitespace(std::string &s) {
   s.erase(std::remove_if(s.begin(), s.end(), is_space), s.end());
